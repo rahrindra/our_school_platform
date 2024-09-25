b017d58c089dca2af1a3fe638014a5415ad64ee7
Rindra <rahrindra@gmail.com>
2024-09-25 20:59:00 +0200

    install & config securty component

---
+++ src/Controller/ApiLoginController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Symfony\Component\Security\Http\Attribute\CurrentUser;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Attribute\Route;
+
+class ApiLoginController extends AbstractController
+{
+    #[Route('/api/login', name: 'api_login', methods: ['POST'])]
+    public function index(#[CurrentUser] ?User $user): JsonResponse
+    {
+        if (null === $user) {
+            return $this->json([
+                'message' => 'missing credentials',
+             ], Response::HTTP_UNAUTHORIZED);
+        }
+
+        $token = "tokenTest";
+
+        return $this->json([
+            'user'  => $user->getUserIdentifier(),
+             'token' => $token,
+        ]);
+    }
+}
